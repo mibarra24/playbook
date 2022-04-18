4401ae169bb6a39c9d2e055ebd027606c2b5f1f5
mibarra24 <79391139+mibarra24@users.noreply.github.com>
2022-04-18 10:00:05 -0700

    commit to main

--- weekly_mission_2/exercises/exercise_2.js
+++ weekly_mission_2/exercises/exercise_2.js
@@ -61,29 +61,37 @@ const explorers = [
 
 console.log("Sobre esta lista, realiza lo siguiente:")
 
-console.log("Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
+console.log("Ejercicio 2.1: Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH")
 
-explorers.forEach(name => console.log(name))
+explorers.forEach(explorer => console.log(explorer.name))
 
-console.log("Imprime el stack de cada explorer, usa FOR EACH")
+console.log("Ejercicio 2.2: Imprime el stack de cada explorer, usa FOR EACH")
 
-explorers.forEach(stack => console.log(stack))
+explorers.forEach(item => console.log(item.stack))
 
-console.log("Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
+console.log("Ejercicio 2.3: Crea una nueva lista con las listas de stacks de cada explorer, usa MAP")
 
+const explorers_stacks = explorers.map(my_explorer_in_list => my_explorer_in_list.stack)
+console.log(explorers_stacks)
 
+console.log("Ejercicio 2.4: Obtén la lista de explorers que tengan en su stack js, usa FILTER (para validar un elemento en un lista se usa el método includes)")
 
-console.log("Obtén la lista de explorers que tengan en su stack js, usa FILTER (para validar un elemento en un lista se usa el método includes)")
+const explorers_with_js = explorers.filter(my_explorer_in_list => my_explorer_in_list.stack.includes("js"))
+console.log(explorers_with_js)
 
-console.log("Busca el primer explorer que sea de la CDMX, usa FIND")
+console.log("Ejercicio 2.5: Busca el primer explorer que sea de la CDMX, usa FIND")
 
+const first_explorer_in_CDMX =explorers.find(my_explorer_in_list => my_explorer_in_list.city == "CDMX")
+console.log(first_explorer_in_CDMX)
 
+console.log("Ejercicio 2.6: Obtén la suma de todos los exercises_completed, usa REDUCE")
 
-console.log("Obtén la suma de todos los exercises_completed, usa REDUCE")
+const all_exercises = explorers.reduce((acc, my_explorer_in_list) => acc + my_explorer_in_list.exercises_completed,0)
+console.log(all_exercises)
 
-console.log("Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME")
+console.log("Ejercicio 2.7: Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME")
 
-console.log("Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY")
+console.log("Ejercicio 2.8: Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY")
 
 
 
